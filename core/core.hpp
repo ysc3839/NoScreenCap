5c8865c446e5bd5cfcc32020b90bfda3a15cdde3
Richard Yu <yurichard3839@gmail.com>
2018-09-23 17:01:49 +0800

    Add wow64 process support in x64 version.

--- core/core.hpp
+++ core/core.hpp
@@ -2,13 +2,59 @@
 
 #include "shellcode.hpp"
 
-#ifdef _WIN64
-#define CODE_SIZE CODE_X64_SIZE
-#define BuildCode(f, a, b) BuildX64Code(f, a)
-#else
+#ifndef _WIN64
 #define CODE_SIZE CODE_X86_SIZE
 #define BuildCode BuildX86Code
-#endif
+#endif // _WIN64
+
+HANDLE WriteAndExecuteCode(HANDLE hProcess, void *code_address, void *code, size_t code_size, void *parameter = nullptr)
+{
+	if (WriteProcessMemory(hProcess, code_address, code, code_size, nullptr))
+		return CreateRemoteThread(hProcess, nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(code_address), parameter, 0, nullptr);
+	return nullptr;
+}
+
+bool WriteAndExecuteCodeWait(HANDLE hProcess, void *code_address, void *code, size_t code_size, void *parameter = nullptr, DWORD timeout = INFINITE)
+{
+	HANDLE hThread = WriteAndExecuteCode(hProcess, code_address, code, code_size, parameter);
+	if (hThread)
+	{
+		WaitForSingleObject(hThread, timeout);
+		CloseHandle(hThread);
+		return true;
+	}
+	return false;
+}
+
+bool WriteAndExecuteCodeWaitReturn(HANDLE hProcess, void *code_address, void *code, size_t code_size, DWORD *exit_code, void *parameter = nullptr, DWORD timeout = INFINITE)
+{
+	HANDLE hThread = WriteAndExecuteCode(hProcess, code_address, code, code_size, parameter);
+	if (hThread)
+	{
+		WaitForSingleObject(hThread, timeout);
+		GetExitCodeThread(hThread, exit_code);
+		CloseHandle(hThread);
+		return true;
+	}
+	return false;
+}
+
+#ifdef _WIN64
+uint32_t GetFuncAddressX86(HANDLE hProcess)
+{
+	uint32_t retval = 0;
+	void *code_address = VirtualAllocEx(hProcess, nullptr, CODE_GET_FUNC_ADDRESS_X86_SIZE_ALL, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (code_address)
+	{
+		BuildGetFuncCode(code_address);
+		DWORD exit_code;
+		if (WriteAndExecuteCodeWaitReturn(hProcess, code_address, code, CODE_GET_FUNC_ADDRESS_X86_SIZE_ALL, &exit_code))
+			retval = exit_code;
+		VirtualFreeEx(hProcess, code_address, CODE_GET_FUNC_ADDRESS_X86_SIZE_ALL, MEM_DECOMMIT);
+	}
+	return retval;
+}
+#endif // _WIN64
 
 bool SetWindowDisplayAffinityForExternelProcess(HWND hWnd, DWORD affinity)
 {
@@ -25,22 +71,32 @@ bool SetWindowDisplayAffinityForExternelProcess(HWND hWnd, DWORD affinity)
 			HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, process_id);
 			if (hProcess)
 			{
+#ifdef _WIN64
+				BOOL is_x86_process = false;
+				if (!IsWow64Process(hProcess, &is_x86_process))
+					is_x86_process = false;
+
+				const size_t CODE_SIZE = is_x86_process ? CODE_X86_SIZE : CODE_X64_SIZE;
+				void *func_addr = is_x86_process ? reinterpret_cast<void*>(static_cast<size_t>(GetFuncAddressX86(hProcess))) : reinterpret_cast<void*>(SetWindowDisplayAffinity);
+#else
 				void *func_addr = reinterpret_cast<void*>(SetWindowDisplayAffinity);
+#endif // _WIN64
 				void *code_address = VirtualAllocEx(hProcess, nullptr, CODE_SIZE, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 				if (code_address)
 				{
-					BuildCode(func_addr, affinity, code_address);
-					if (WriteProcessMemory(hProcess, code_address, code, CODE_SIZE, nullptr))
+#ifdef _WIN64
+					if (!is_x86_process)
 					{
-						HANDLE hThread = CreateRemoteThread(hProcess, nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(code_address), hWnd, 0, nullptr);
-						if (hThread)
-						{
-							WaitForSingleObject(hThread, INFINITE);
-							CloseHandle(hThread);
-							retval = true;
-						}
+						BuildX64Code(func_addr, affinity);
+						retval = WriteAndExecuteCodeWait(hProcess, code_address, code, CODE_SIZE, hWnd);
 					}
-					VirtualFreeEx(hProcess, code_address, sizeof(code), MEM_DECOMMIT);
+					else
+#endif // _WIN64
+					{
+						BuildX86Code(func_addr, affinity, code_address);
+						retval = WriteAndExecuteCodeWait(hProcess, code_address, code, CODE_SIZE, hWnd);
+					}
+					VirtualFreeEx(hProcess, code_address, CODE_SIZE, MEM_DECOMMIT);
 				}
 				CloseHandle(hProcess);
 			}
--- core/shellcode.hpp
+++ core/shellcode.hpp
@@ -28,3 +28,79 @@ constexpr auto CODE_X86_SIZE = sizeof(_CODE_X86);
 	uint8_t code[] = { CODE_X86 }; \
 	code[3] = static_cast<uint8_t>(affinity); \
 	*reinterpret_cast<void**>(&code[7]) = reinterpret_cast<void*>(reinterpret_cast<size_t>(func_addr) - (reinterpret_cast<size_t>(base_address) + 6) - 5);
+
+// Source code: shellcode_x86_get_func_address.c
+#define CODE_GET_FUNC_ADDRESS_X86 \
+	0x64, 0xA1, 0x30, 0x00, 0x00, 0x00,	/* mov eax,dword ptr fs:[00000030h] */ \
+	0x53,								/* push ebx */ \
+	0x56,								/* push esi */ \
+	0x57,								/* push edi */ \
+	0x8B, 0x40, 0x0C,					/* mov eax,dword ptr [eax+0Ch] */ \
+	0x8B, 0x40, 0x14,					/* mov eax,dword ptr [eax+14h] */ \
+	0x8B, 0x00,							/* mov eax,dword ptr [eax] */ \
+	0x8B, 0x00,							/* mov eax,dword ptr [eax] */ \
+	0x8B, 0x78, 0x10,					/* mov edi,dword ptr [eax+10h] */ \
+	0x8B, 0x47, 0x3C,					/* mov eax,dword ptr [edi+3Ch] */ \
+	0x8B, 0x44, 0x38, 0x78,				/* mov eax,dword ptr [eax+edi+78h] */ \
+	0x8B, 0x4C, 0x38, 0x20,				/* mov ecx,dword ptr [eax+edi+20h] */ \
+	0x8B, 0x74, 0x38, 0x24,				/* mov esi,dword ptr [eax+edi+24h] */ \
+	0x03, 0xCF,							/* add ecx,edi */ \
+	0x8B, 0x5C, 0x38, 0x1C,				/* mov ebx,dword ptr [eax+edi+1Ch] */ \
+	0x03, 0xF7,							/* add esi,edi */ \
+	0x03, 0xDF,							/* add ebx,edi */ \
+	0x33, 0xD2,							/* xor edx,edx */ \
+	/* label1: */ \
+	0x8B, 0x01,							/* mov eax,dword ptr [ecx] */ \
+	0x81, 0x3C, 0x38, 0x47, 0x65, 0x74, 0x50, /* cmp dword ptr [eax+edi],50746547h */ \
+	0x75, 0x14,							/* jne <label2> */ \
+	0x81, 0x7C, 0x38, 0x04, 0x72, 0x6F, 0x63, 0x41, /* cmp dword ptr [eax+edi+4],41636F72h */ \
+	0x75, 0x0A,							/* jne <label2> */ \
+	0x81, 0x7C, 0x38, 0x08, 0x64, 0x64, 0x72, 0x65, /* cmp dword ptr [eax+edi+8],65726464h */ \
+	0x74, 0x06,							/* je <label3> */ \
+	/* label2: */ \
+	0x83, 0xC1, 0x04,					/* add ecx,4 */ \
+	0x42,								/* inc edx */ \
+	0xEB, 0xDB,							/* jmp <label1> */ \
+	/* label3: */ \
+	0x0F, 0xB7, 0x04, 0x56,				/* movzx eax,word ptr [esi+edx*2] */ \
+	0x68, 0x00, 0x00, 0x00, 0x00,		/* push offset string "GetModuleHandleA" */ \
+	0x57,								/* push edi */ \
+	0x8B, 0x34, 0x83,					/* mov esi,dword ptr [ebx+eax*4] */ \
+	0x03, 0xF7,							/* add esi,edi */ \
+	0xFF, 0xD6,							/* call esi */ \
+	0x68, 0x00, 0x00, 0x00, 0x00,		/* push offset string "SetWindowDisplayAffinity" */ \
+	0x68, 0x00, 0x00, 0x00, 0x00,		/* push offset string "user32" */ \
+	0xFF, 0xD0,							/* call eax */ \
+	0x50,								/* push eax */ \
+	0xFF, 0xD6,							/* call esi */ \
+	0x5F,								/* pop edi */ \
+	0x5E,								/* pop esi */ \
+	0x5B,								/* pop ebx */ \
+	0xC2, 0x04, 0x00					/* ret 4 */ \
+
+#define CODE_GET_FUNC_ADDRESS_X86_DATA_GMH \
+	'G','e','t','M','o','d','u','l','e','H','a','n','d','l','e','A',0
+#define CODE_GET_FUNC_ADDRESS_X86_DATA_USER32 \
+	'u','s','e','r','3','2',0
+#define CODE_GET_FUNC_ADDRESS_X86_DATA_SWDF \
+	'S','e','t','W','i','n','d','o','w','D','i','s','p','l','a','y','A','f','f','i','n','i','t','y',0
+
+constexpr uint8_t _CODE_GET_FUNC_ADDRESS_X86[] = { CODE_GET_FUNC_ADDRESS_X86 };
+constexpr auto CODE_GET_FUNC_ADDRESS_X86_SIZE = sizeof(_CODE_GET_FUNC_ADDRESS_X86);
+
+constexpr uint8_t _CODE_GET_FUNC_ADDRESS_X86_DATA_GMH[] = { CODE_GET_FUNC_ADDRESS_X86_DATA_GMH };
+constexpr auto CODE_GET_FUNC_ADDRESS_X86_DATA_GMH_SIZE = sizeof(_CODE_GET_FUNC_ADDRESS_X86_DATA_GMH);
+
+constexpr uint8_t _CODE_GET_FUNC_ADDRESS_X86_DATA_USER32[] = { CODE_GET_FUNC_ADDRESS_X86_DATA_USER32 };
+constexpr auto CODE_GET_FUNC_ADDRESS_X86_DATA_USER32_SIZE = sizeof(_CODE_GET_FUNC_ADDRESS_X86_DATA_USER32);
+
+constexpr uint8_t _CODE_GET_FUNC_ADDRESS_X86_DATA_SWDF[] = { CODE_GET_FUNC_ADDRESS_X86_DATA_SWDF };
+constexpr auto CODE_GET_FUNC_ADDRESS_X86_DATA_SWDF_SIZE = sizeof(_CODE_GET_FUNC_ADDRESS_X86_DATA_SWDF);
+
+constexpr auto CODE_GET_FUNC_ADDRESS_X86_SIZE_ALL = CODE_GET_FUNC_ADDRESS_X86_SIZE + CODE_GET_FUNC_ADDRESS_X86_DATA_GMH_SIZE + CODE_GET_FUNC_ADDRESS_X86_DATA_USER32_SIZE + CODE_GET_FUNC_ADDRESS_X86_DATA_SWDF_SIZE;
+
+#define BuildGetFuncCode(base_address) \
+	uint8_t code[] = { CODE_GET_FUNC_ADDRESS_X86, CODE_GET_FUNC_ADDRESS_X86_DATA_GMH, CODE_GET_FUNC_ADDRESS_X86_DATA_USER32, CODE_GET_FUNC_ADDRESS_X86_DATA_SWDF }; \
+	*reinterpret_cast<uint32_t*>(&code[91]) = static_cast<uint32_t>(reinterpret_cast<size_t>(base_address) + CODE_GET_FUNC_ADDRESS_X86_SIZE); \
+	*reinterpret_cast<uint32_t*>(&code[104]) = static_cast<uint32_t>(reinterpret_cast<size_t>(base_address) + CODE_GET_FUNC_ADDRESS_X86_SIZE + CODE_GET_FUNC_ADDRESS_X86_DATA_GMH_SIZE + CODE_GET_FUNC_ADDRESS_X86_DATA_USER32_SIZE); \
+	*reinterpret_cast<uint32_t*>(&code[109]) = static_cast<uint32_t>(reinterpret_cast<size_t>(base_address) + CODE_GET_FUNC_ADDRESS_X86_SIZE + CODE_GET_FUNC_ADDRESS_X86_DATA_GMH_SIZE);
